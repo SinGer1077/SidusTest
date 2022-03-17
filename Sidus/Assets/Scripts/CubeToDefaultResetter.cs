92db7e70749e15af03711dc2478d1946ef978c24
MikhailShevchuk <cp91@mail.ru>
2022-03-17 23:42:30 +0500

    Change class name

--- Sidus/Assets/Scripts/CubeToDefaultResetter.cs
+++ Sidus/Assets/Scripts/CubeToDefaultResetter.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingCubeToDefaultResetter : MonoBehaviour
+public class CubeToDefaultResetter : MonoBehaviour
 {
     private Vector3 _defaultPosition;
 
