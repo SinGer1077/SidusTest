a279f6005e9835719ea9b38b65a05ec86feaf4f6
MikhailShevchuk <cp91@mail.ru>
2022-03-17 18:58:22 +0500

    Add bezier get point function

---
+++ Sidus/Assets/Scripts/CubeMoving.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoving : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _movingCube;
+
+    public void MoveCube()
+    {
+
+    }
+}
